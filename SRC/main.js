20ea17ce8f505b7cfa0663105ec4f2624587eab8
Avocado <yauness190@gmail.com>
2023-09-30 21:12:31 +0100

    timer problem needs to be fixed

--- SRC/main.js
+++ SRC/main.js
@@ -2,38 +2,15 @@
 const holder = document.getElementById("holder");
 const winMark = ["x", "o"];
 const boxs = document.getElementsByClassName("box");
-const winSpots = {row:[[1,2,3], [4,5,6], [7,8,9]],column:[[1,4,7],[2,5,8],[3,6,9]],cross:[[1,5,9],[3,5,7]]}
+const winSpots = {row:[[1,2,3], [4,5,6], [7,8,9]],column:[[1,4,7],[2,5,8],[3,6,9]],cross:[[1,5,9],[3,5,7]]};
+let board = document.createElement("div");
+board.className = "board";
 let x = 1;
+var stop = false;
 
-
-// Promise
-function watchForRetry() {
-  return new Promise((resolve, reject) => {
-  const isEnd = document.querySelector(".finish");
-  if (isEnd) {
-    resolve("Retry");
-  }else {
-    reject(timer());
-  }
-})}
-watchForRetry().then((message) => {
-  const endMsg = document.createElement("button");
-  const endMsgText = document.createTextNode(`${message}`);
-  endMsg.appendChild(endMsgText);
-  endMsg.className = "end-msg";
-  document.body.appendChild(endMsg);
-}).catch((message) => {
-  let endMsgText = document.createTextNode(`${message} ${date()}`);
-  const endMsg = document.querySelector(".on-game-msg");
-  if (endMsg) endMsg.innerText = endMsgText.nodeValue;
-  if (!endMsg) {
-    const endMsg = document.createElement("div");
-    endMsg.appendChild(endMsgText);
-    endMsg.className = "on-game-msg";
-    document.body.appendChild(endMsg);
-  }
-})
-
+// append board
+document.body.appendChild(board);
+debugger;
 
 // loop for creating boxs with a special classes
 for (let i = 1; i < 10; i++) {
@@ -64,7 +41,30 @@ for (let i of boxs) {
       if (x%2 !== 0) mark = "x";
       if (x%2 === 0) mark = "o";
       box.classList.add("done", mark);
-      checkWin();
+
+      function watchForRetry() {
+        return new Promise((resolve, reject) => {
+          let isEnd = restart();
+          let isWin = Boolean(checkWin());
+          console.log(board.classList.contains("on-game"))
+        if (isEnd || isWin) {
+          resolve("Retry");
+        }else {
+          reject();
+        }
+      })}
+      watchForRetry().then((message) => {
+        stop = true;
+        board.classList.remove("on-game");
+        board.classList.add("end")
+        board.innerHTML = message;
+        win();
+      }).catch(() => { // no "on-game", yes "end", 
+        if ((!board.classList.contains("on-game")) && (!board.classList.contains("end"))) {
+          board.classList.add("on-game");
+          win();
+        }
+      })
       return x++;
     }
   }
@@ -73,8 +73,7 @@ for (let i of boxs) {
 // Functions
 // remove all classes that been added during the game
 function reset(token= "itcantbeanylementnamedlikethis") {
-  console.log("reset")
-  for (let box of boxs) {
+    for (let box of boxs) {
     for (let mark of winMark) {
       box.classList.remove(mark, "done", token);
     }
@@ -83,15 +82,22 @@ function reset(token= "itcantbeanylementnamedlikethis") {
 
 // resetart automaticly when all boxs are filled with a draw result
 function restart() {
+  let isEnd = false;
   let num = 0;
   for (let box of boxs) {
     if (box.classList.contains("x") && box.classList.contains("done") || box.classList.contains("o") && box.classList.contains("done")) num++;
   }
-  if (num === (boxs.length)) reset();
+  if (num === (boxs.length)) {
+    reset();
+    return  isEnd = true;
+  };
+  return isEnd
 }
 
 // preper all win conditions
 function checkWin() {
+  let isEnd = "";
+
   let boxCrossL = document.getElementsByClassName("cross1");
   let boxCrossR = document.getElementsByClassName("cross2");
   let boxCol1 = document.getElementsByClassName("column1");
@@ -101,48 +107,26 @@ function checkWin() {
   let boxRow2 = document.getElementsByClassName("row2");
   let boxRow3 = document.getElementsByClassName("row3");
 
-  function counterInGame() {
-    let endMsgText = document.createTextNode(`${date()}`);
-    const endMsg = document.querySelector(".on-game-msg");
-    if (endMsg) endMsg.innerText = endMsgText.nodeValue;
-    if (!endMsg) {
-      const endMsg = document.createElement("div");
-      endMsg.appendChild(endMsgText);
-      endMsg.className = "on-game-msg";
-      document.body.appendChild(endMsg);
-    }
-  }
-
-
-  function watchForRetry() {
-    return new Promise((resolve, reject) => {
-    const isEnd = document.querySelector(".finish");
-    if (isEnd) {
-      resolve("Retry");
-    }else {
-      reject();
-    }
-  })}
-  watchForRetry().catch((message) => {
-  })
-  // timer()
-
   for (let i of winMark) {
-    if (boxCrossL[0].classList.contains(i) && boxCrossL[1].classList.contains(i) && boxCrossL[2].classList.contains(i)) win(i);
-    if (boxCrossR[0].classList.contains(i) && boxCrossR[1].classList.contains(i) && boxCrossR[2].classList.contains(i)) win(i);
-    if (boxCol1[0].classList.contains(i) && boxCol1[1].classList.contains(i) && boxCol1[2].classList.contains(i)) win(i);
-    if (boxCol2[0].classList.contains(i) && boxCol2[1].classList.contains(i) && boxCol2[2].classList.contains(i)) win(i);
-    if (boxCol3[0].classList.contains(i) && boxCol3[1].classList.contains(i) && boxCol3[2].classList.contains(i)) win(i);
-    if (boxRow1[0].classList.contains(i) && boxRow1[1].classList.contains(i) && boxRow1[2].classList.contains(i)) win(i);
-    if (boxRow2[0].classList.contains(i) && boxRow2[1].classList.contains(i) && boxRow2[2].classList.contains(i)) win(i);
-    if (boxRow3[0].classList.contains(i) && boxRow3[1].classList.contains(i) && boxRow3[2].classList.contains(i)) win(i);
+    if (boxCrossL[0].classList.contains(i) && boxCrossL[1].classList.contains(i) && boxCrossL[2].classList.contains(i)) isEnd = i;
+    if (boxCrossR[0].classList.contains(i) && boxCrossR[1].classList.contains(i) && boxCrossR[2].classList.contains(i)) isEnd = i;
+    if (boxCol1[0].classList.contains(i) && boxCol1[1].classList.contains(i) && boxCol1[2].classList.contains(i)) isEnd = i;
+    if (boxCol2[0].classList.contains(i) && boxCol2[1].classList.contains(i) && boxCol2[2].classList.contains(i)) isEnd = i;
+    if (boxCol3[0].classList.contains(i) && boxCol3[1].classList.contains(i) && boxCol3[2].classList.contains(i)) isEnd = i;
+    if (boxRow1[0].classList.contains(i) && boxRow1[1].classList.contains(i) && boxRow1[2].classList.contains(i)) isEnd = i;
+    if (boxRow2[0].classList.contains(i) && boxRow2[1].classList.contains(i) && boxRow2[2].classList.contains(i)) isEnd = i;
+    if (boxRow3[0].classList.contains(i) && boxRow3[1].classList.contains(i) && boxRow3[2].classList.contains(i)) isEnd = i;
+    if (isEnd !== "") return isEnd;
   }
   restart();
+  return isEnd
 }
 
 // do an action after define a win case
-function win(mark) {
+function win() {
   // this part is just for styling
+  if (checkWin() === "") return;
+  let mark = checkWin();
   let bg = "";
   if (mark === "x") bg = "rgb(7, 16, 24)";
   if (mark === "o") bg = "#e91e63a6";
@@ -150,24 +134,6 @@ function win(mark) {
     box.classList.add("finish");
   }
 
-  function watchForRetry() {
-    return new Promise((resolve, reject) => {
-    const isEnd = document.querySelector(".finish");
-    if (isEnd) {
-      resolve("Retry");
-    }else {
-      reject("Stop_Watch");
-    }
-  })}
-  watchForRetry().then((message) => {
-    const endMsg = document.createElement("button");
-    const endMsgText = document.createTextNode(`${message}`);
-    endMsg.appendChild(endMsgText);
-    endMsg.className = "end-msg";
-    document.body.appendChild(endMsg);
-    let del = document.querySelector(".on-game-msg");
-    del.remove()
-  })
 
   // use the winner mark as a text at the end
   mark = mark.toUpperCase();
@@ -193,54 +159,33 @@ function objlenght(obj) {
 // Events
 
 document.addEventListener("click", (element) =>  {
-  if (element.target.className === "end-msg") {
+  if (element.target.classList.contains("end")) {
     reset("finish")
     document.querySelector(".win").remove();
-    document.querySelector(".end-msg").remove();
-    function watchForRetry() {
-      return new Promise((resolve, reject) => {
-      const isEnd = document.querySelector(".finish");
-      if (isEnd) {
-        resolve("Retry");
-      }else {
-        reject("Stop_Watch");
-      }
-    })}
-    watchForRetry().catch((message) => {
-      let endMsgText = document.createTextNode(`${message} ${date()}`);
-      const endMsg = document.querySelector(".on-game-msg");
-      if (endMsg) endMsg.innerText = endMsgText.nodeValue;
-      if (!endMsg) {
-        const endMsg = document.createElement("div");
-        endMsg.appendChild(endMsgText);
-        endMsg.className = "on-game-msg";
-        document.body.appendChild(endMsg);
-        setInterval(timer, 1000)
-      }
-    })
+    // document.querySelector(".end").remove();
   };
 })
 
 // work on a timer
 
-const onGame = document.querySelector(".on-game-msg");
-function timer() {
-  let mainPlace = document.querySelector(".on-game-msg");
-  if (!mainPlace) return;
-  let s = 0;
-  let min = 0;
-  let zeroS = 0;
-  let zeroM = 0;
-  function counter() {
-    if (s < 60) s++;
-    if (s === 60) {
-      s = 0;
-      min++
-    }
-    let pText = `${zeroM}${min} : ${zeroS}${s}`;
-    if (s >= 10) pText = `${zeroM}${min} : ${s}`;// it's sitll need to deal with the min numbers
-    mainPlace.innerHTML = pText;
+let s = 0;
+let min = 0;
+let zeroS = 0;
+let zeroM = 0;
+function counter(element) {
+  if (s < 60) s++;
+  if (s === 60) {
+    s = 0;
+    min++
   }
-  setInterval(counter, 1000)
+  let pText = `${zeroM}${min} : ${zeroS}${s}`;
+  if (s >= 10) pText = `${zeroM}${min} : ${s}`;// it's sitll need to deal with the min numbers
+  return element.innerHTML = pText;
 }
-
+setInterval(() => {
+  if (!board.classList.contains("on-game")) return;
+  counter(board);
+  if (board.classList.contains("end")) {
+    clearInterval(1);
+  }
+}, 1000);
